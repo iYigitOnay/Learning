ecfb2378d11ab951875d1981c5e295f5fb298ceb
Yiğit Önay <mavi221907@gmail.com>
2025-10-10 11:52:25 +0300

    read on less
    read on less

---
+++ PythonLearn/10_10-pyLes/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print(np.array([1,2,3]))
+print(np.shape(np.array([[[1,2], [3,4]], [[5,6], [7,8]]])))
+
+np.random.seed(0)
+print(np.random.randint(0, 100, 10))
+print(np.random.normal(0, 1, 10))
+
+print("NumPy Bir Soru Kesin Çıkıcak \n Normal Dağılım Nedir bilinmesi gerek sınavda çıkabilir \n")
